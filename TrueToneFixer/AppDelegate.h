d7f590c81cd7da763a1d4113f6f2d2ce0744a46c
Grant Hobbs <grant@gwhobbs.com>
2022-03-19 12:29:00 -0500

    Initial Commit

---
+++ TrueToneFixer/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TrueToneFixer
+//
+//  Created by Grant Hobbs on 3/19/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TrueToneFixer/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  TrueToneFixer
+//
+//  Created by Grant Hobbs on 3/19/22.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
